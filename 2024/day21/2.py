c963e1c83f302a37e907ebddeab7145bb23b0e1a
brechtlauwers <brechtlauwers02@gmail.com>
2024-12-23 21:27:46 +0100

    Finally made 21P2

--- 2024/day21/2.py
+++ 2024/day21/2.py
@@ -1,28 +1,12 @@
-from queue import Queue
-from itertools import permutations
 from functools import cache
 from itertools import pairwise
-
-
 
 with open("input.txt") as file_in:
     codes = []
     for line in file_in:
         codes.append(line.strip())
 
-# num_keypad = {'0': [('A', '>'), ('2', '^')],
-#               'A': [('0', '<'), ('3', '^')],
-#               '1': [('2', '>'), ('4', '^')],
-#               '2': [('1', '<'), ('5', '^'), ('3', '>'), ('0', 'v')],
-#               '3': [('2', '<'), ('6', '^'), ('A', 'v')],
-#               '4': [('5', '>'), ('7', '^'), ('1', 'v')],
-#               '5': [('4', '<'), ('8', '^'), ('2', 'v'), ('6', '>')],
-#               '6': [('5', '<'), ('9', '^'), ('3', 'v')],
-#               '7': [('8', '>'), ('4', 'v')],
-#               '8': [('7', '<'), ('5', 'v'), ('9', '>')],
-#               '9': [('8', '<'), ('6', 'v')],
-#               'A': [('0', '<'), ('3', '^')]}
-
+# All possible mappings for the numerical keypad
 num_keypad = {'A': {'A': 'A', '0': '<A', '1': '^<<A', '2': '<^A', '3': '^A', '4': '^^<<A', '5': '<^^A', '6': '^^A', '7': '^^^<<A', '8': '<^^^A', '9': '^^^A'},
  '0': {'A': '>A', '0': 'A', '1': '^<A', '2': '^A', '3': '^>A', '4': '^^<A', '5': '^^A', '6': '^^>A', '7': '^^^<A', '8': '^^^A', '9': '^^^>A'},
  '1': {'A': '>>vA', '0': '>vA', '1': 'A', '2': '>A', '3': '>>A', '4': '^A', '5': '^>A', '6': '^>>A', '7': '^^A', '8': '^^>A', '9': '^^>>A'},
@@ -36,74 +20,39 @@ num_keypad = {'A': {'A': 'A', '0': '<A', '1': '^<<A', '2': '<^A', '3': '^A', '4'
  '9': {'A': 'vvvA', '0': '<vvvA', '1': '<<vvA', '2': '<vvA', '3': 'vvA', '4': '<<vA', '5': '<vA', '6': 'vA', '7': '<<A', '8': '<A', '9': 'A'}}
 
 # All possible mappings for (from, to): path
-direct_keypad = {('<', 'v'): ['>'],
-                 ('<', '^'): ['>^'],
-                 ('<', '>'): ['>>'],
-                 ('<', 'A'): ['>^>', '>>^'],
-                 ('<', '<'): [''],
-                 ('v', '<'): ['<'],
-                 ('v', '^'): ['^'],
-                 ('v', '>'): ['>'],
-                 ('v', 'A'): ['>^', '^>'],
-                 ('v', 'v'): [''],
-                 ('>', 'A'): ['^'],
-                 ('>', 'v'): ['<'],
-                 ('>', '<'): ['<<'],
-                 ('>', '^'): ['<^', '^<'],
-                 ('>', '>'): [''],
-                 ('^', '<'): ['v<'],
-                 ('^', 'v'): ['v'],
-                 ('^', 'A'): ['>'],
-                 ('^', '>'): ['>v', 'v>'],
-                 ('^', '^'): [''],
-                 ('A', '^'): ['<'],
-                 ('A', '>'): ['v'],
-                 ('A', 'v'): ['<v', 'v<'],
-                 ('A', '<'): ['<v<'],
-                 ('A', 'A'): ['']}
-
-direct_keypad = {'A': {'A': 'A', '<': 'v<<A', '^': '<A', '>': 'vA', 'v': '<vA'},'<': {'A': '>>^A', '<': 'A', '^': '>^A', '>': '>>A', 'v': '>A'},'^': {'A': '>A', '<': 'v<A', '^': 'A', '>': 'v>A', 'v': 'vA'},'>': {'A': '^A', '<': '<<A', '^': '<^A', '>': 'A', 'v': '<A'},'v': {'A': '^>A', '<': '<A', '^': '^A', '>': '>A', 'v': 'A'}}
+direct_keypad = {'A': {'A': 'A', '<': 'v<<A', '^': '<A', '>': 'vA', 'v': '<vA'},
+                 '<': {'A': '>>^A', '<': 'A', '^': '>^A', '>': '>>A', 'v': '>A'},
+                 '^': {'A': '>A', '<': 'v<A', '^': 'A', '>': 'v>A', 'v': 'vA'},
+                 '>': {'A': '^A', '<': '<<A', '^': '<^A', '>': 'A', 'v': '<A'},
+                 'v': {'A': '^>A', '<': '<A', '^': '^A', '>': '>A', 'v': 'A'}}
 
 
 def enter_code(code):
     code = "A" + code
-    path = ""
-    for i in range(len(code) - 1):
-        path += num_keypad[code[i]][code[i+1]]
-    return path
+    path = 0
+    initial_code = ""
+    for buttons in pairwise(code):
+        initial_code += num_keypad[buttons[0]][buttons[1]]
+    return initial_code
 
-# def enter_direct_code(code):
-#     code = "A" + code
-#     path = ""
-#     for i in range(len(code) - 1):
-#         path += direct_keypad[code[i]][code[i+1]]
-#     return path
+@cache
+def enter_button(code, depth):
+    if depth == 0:
+        return len(code)
+    total = 0
+    for b in pairwise("A" + code):
+        total += enter_button(direct_keypad[b[0]][b[1]], depth - 1)
+    return total
 
-def enter_direct_code(code):
-    code = "A" + code
-    n = len(code)
-    result = bytearray()  # Use a bytearray for efficient string concatenation
-    append = result.extend  # Avoid attribute lookups in the loop
-
-    for i in range(n - 1):
-        append(direct_keypad[code[i]][code[i + 1]].encode())
-
-    return result.decode()
 
 total = 0
 for code in codes:
     direct_code = enter_code(code)
+    final_len = enter_button(direct_code, 25)
     
-    for _ in range(25):
-        direct_code = enter_direct_code(direct_code)
-        print(_, len(direct_code))
-        
-    print(f"{code}: {final_code}")
-    code_len = len(final_code)
+    print(f"{code}: {final_len}")
     num_part = int(code[:3])
-    print(code_len, num_part)
-    
-    total += code_len * num_part
+    total += final_len * num_part
 
 print(total)
 
